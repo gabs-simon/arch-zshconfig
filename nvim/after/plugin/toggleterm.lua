4fc25050e34e4f0f5dd680c2b42c58cae0f8abea
Gabs Simonetti <gabs@invoicesimple.com>
2024-01-11 01:34:19 -0800

    Update .gitignore and Neovim toggleterm config; streamline zsh plugins
     - Add `shell_gpt/` and `gh*` patterns to .gitignore to prevent tracking.
     - Modify Neovim's `toggleterm.lua` to set keymaps for normal and terminal modes to open floating terminal.
     - Remove configuration and sourcing for `zsh-histdb` from `zsh/plugins.sh`, simplifying plugin setup.

--- nvim/after/plugin/toggleterm.lua
+++ nvim/after/plugin/toggleterm.lua
@@ -1,1 +1,2 @@
-vim.keymap.set("n", "<C-\\>", vim.cmd.ToggleTerm)
+vim.keymap.set("n", "<C-\\>", function () vim.cmd("ToggleTerm direction=float") end)
+vim.keymap.set("t", "<C-\\>", function () vim.cmd("ToggleTerm direction=float") end)
--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -7,10 +7,3 @@ source ~/.zpm/zpm.zsh
 
 zpm load zsh-users/zsh-autosuggestions
 zpm load zdharma-continuum/fast-syntax-highlighting
-
-HISTDB_TABULATE_CMD=(sed -e $'s/\x1f/\t/g')
-
-zpm load larkery/zsh-histdb
-source ~/.zpm/plugins/larkery---zsh-histdb/sqlite-history.zsh
-autoload -Uz add-zsh-hook
-
