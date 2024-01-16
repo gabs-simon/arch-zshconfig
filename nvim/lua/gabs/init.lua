2591e956bf701e7e109226647faaa0299e52abbf
Gabs Simonetti <gabs.simon@outlook.com>
2024-01-15 16:12:23 -0800

    tab settings

--- nvim/lua/gabs/init.lua
+++ nvim/lua/gabs/init.lua
@@ -1,11 +1,12 @@
 vim.g.mapleader = " "
 vim.opt.scrolloff = 10
-vim.opt.tabstop = 2
+vim.opt.tabstop = 4
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "80,120"
 vim.opt.cursorline = true
+vim.opt.smarttab = true
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
