2cb2abada85eb78aff9b895fe55a4d0fec15702f
Gabs Simonetti <gabs@invoicesimple.com>
2024-01-22 13:03:47 -0800

    Add descriptive labels to Telescope key mappings in Neovim config

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,11 +1,11 @@
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
-vim.keymap.set("n", "<leader>fl", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Find Git Files" })
+vim.keymap.set("n", "<leader>fl", builtin.live_grep, { desc = "Live Grep" })
 vim.keymap.set("n", "<leader>fs", function()
   builtin.grep_string({ search = vim.fn.input("Grep String > ") });
-end)
+end, { desc = "Fuzzy find using grep string" })
 
 require('telescope').load_extension('fzf')
 require('telescope').load_extension('media_files')
