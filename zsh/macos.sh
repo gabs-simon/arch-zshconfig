59a1b8169721ec162d94ef5e797359a4a92e2470
Gabs Simonetti <gabs.simonetti@outlook.com>
2024-06-27 12:56:12 -0700

    fixed macos android build

--- zsh/macos.sh
+++ zsh/macos.sh
@@ -1,7 +1,10 @@
-export PATH="/opt/homebrew/opt/openjdk@11/bin:$PATH"
-ANDROID_HOME=$HOME/Library/Android/sdk
-
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+export PATH="$PATH:/opt/homebrew/opt/openjdk@11/bin"
+export PATH="$PATH:$ANDROID_HOME/emulator"
+export PATH="$PATH:$ANDROID_HOME/platform-tools"
 export PATH="$PATH:/opt/homebrew/opt/python@3.11/libexec/bin" 
 
 export PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
-export PATH="$HOME/Library/Android/sdk/build-tools/34.0.0/:$HOME/Library/Android/sdk/platform-tools/:$PATH"
+export PATH="$PATH:$HOME/Library/Android/sdk/build-tools/34.0.0"
+
+export JAVA_HOME="/opt/homebrew/Cellar/openjdk@11/11.0.23/libexec/openjdk.jdk/Contents/Home"
